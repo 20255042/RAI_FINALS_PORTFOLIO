7eb33c6e58d779ff2ed3b6bb0062e3406680a765
Sandara <20255042@s.ubaguio.edu>
2026-05-15 15:34:40 +0800

    fix 404 routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
